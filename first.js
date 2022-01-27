0a52a659a5a596a4c778d2fab7f9ea397ef0f9e3
Bipin Maurya <96836433+bipinmaurya0@users.noreply.github.com>
2022-01-27 15:23:46 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,17 @@
+var n1=prompt("please enter a number");
+var n2=prompt("please enter a number");
+var n3=prompt("please enter a number");
+  a=parseInt(n1);
+  b=parseInt(n2);
+  c=parseInt(n3);
+if(a<b && a<c)
+{
+alert(a +"is the smallest no.")
+}
+else if(b<a && b<c)
+{
+alert(b +"is the smallest no.")
+ else
+{
+alert(c +" is the smallest no.")
+}
---
+++ second.js
@@ -0,0 +1,7 @@
+var input=prompt(" please enter a charecter");
+if(input=="a" || input=="A"||input=="e"||input=="E"|| input=="i" || input=="I"|| input=="o" || input=="O" || input=="u" || input=="U"){
+alert(input +" is a vowel");
+}
+else{
+alert(input + "is a consonant");
+}
